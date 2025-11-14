1e86a93c8db730086fccef67bc8f2f6e93406c6a
veimilt <veikko.milton@gmail.com>
2025-11-14 23:29:36 +0200

    fixed middlewares

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-// const middleware = require('./utils/middleware')
+const middleware = require('./utils/middleware')
 const blogslistRouter = require('./controllers/blogs')
 
 const app = express()
@@ -20,8 +20,12 @@ mongoose
 
 app.use(express.static('dist'))
 app.use(express.json())
-// app.use(middleware.requestLogger)
+
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogslistRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
